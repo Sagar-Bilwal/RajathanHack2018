4ffb5db38117c56cdbcbcfcc1959cddf4c5adb43
SAGAR <bilwal.sagar@gmail.com>
2018-03-15 14:08:10 +0530

    Initial commit

---
+++ app/src/main/java/com/example/sagar/myapplication/MainActivity.java
@@ -0,0 +1,109 @@
+package com.example.sagar.myapplication;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    ArrayList<String> titles;
+    NewsAdapter adapter;
+    ArrayList<String> urls;
+    ArrayList<Integer> urlsToImage;
+    ArrayList<String> descriptions;
+    CardView cardView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        RecyclerView recyclerView = findViewById(R.id.recylerView);
+        int[] androidColors = getResources().getIntArray(R.array.androidcolors);
+        titles = new ArrayList<String>();
+        descriptions = new ArrayList<String>();
+
+        urlsToImage = new ArrayList<Integer>();
+
+        cardView = findViewById(R.id.cardView);
+
+        adapter = new NewsAdapter(androidColors, titles, descriptions,urlsToImage, getApplicationContext());
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(adapter);
+
+
+
+        titles.add("Jaipur");
+        titles.add("Udaipur");
+        titles.add("Kota");
+        descriptions.add("Jaipur");
+        descriptions.add("Udaipur");
+        descriptions.add("Kota");
+        urlsToImage.add(R.drawable.japur);
+        urlsToImage.add(R.drawable.udaipur);
+        urlsToImage.add(R.drawable.kota);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
---
+++ app/src/main/java/com/example/sagar/myapplication/NewsAdapter.java
@@ -0,0 +1,88 @@
+package com.example.sagar.myapplication;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+/**
+ * Created by SAGAR on 15-03-2018.
+ */
+
+public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.ViewHolder> {
+    private ArrayList<String> titles;
+    private Context context;
+    private ArrayList<String> descriptions;
+
+    private ArrayList<Integer> urlsToImage;
+
+    private int[] androidColors;
+
+    public NewsAdapter(int[] androidColors, ArrayList<String> titles, ArrayList<String> descriptions, ArrayList<Integer> urlsToImage, Context context) {
+        this.titles = titles;
+        this.descriptions = descriptions;
+
+        this.context = context;
+        this.urlsToImage = urlsToImage;
+        this.androidColors = androidColors;
+    }
+
+    @Override
+    public NewsAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.row_item,parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(NewsAdapter.ViewHolder holder, final int position) {
+        holder.title.setText(titles.get(position));
+        holder.description.setText((descriptions.get(position)));
+
+
+        int randomColor = androidColors[new Random().nextInt(androidColors.length)];
+        holder.linearLayout.setBackgroundColor(randomColor);
+        Picasso.with(context).load(urlsToImage.get(position)).into(holder.imageView);
+        holder.linearLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ///context.startActivity(new Intent(context, Main2Activity.class).putExtra("url", urls.get(position)).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
+                Toast.makeText(context, titles.get(position), Toast.LENGTH_LONG);
+
+            }
+        });
+
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return titles.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        public TextView title, description;
+        public ImageView imageView;
+        public LinearLayout linearLayout;
+
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            title = (TextView) itemView.findViewById(R.id.textView1);
+            imageView = (ImageView) itemView.findViewById(R.id.imageview);
+            linearLayout = (LinearLayout) itemView.findViewById(R.id.linearLayout);
+            description = (TextView) itemView.findViewById(R.id.textView2);
+        }
+    }
+}
+
